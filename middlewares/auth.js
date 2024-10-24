0cc3ee57e27ed1cb5f584a5fa4efb41f173f413d
Frederique <tarroux.frederique@gmail.com>
2024-10-24 18:53:18 +0200

    init back

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,10 +12,11 @@ module.exports = async (req, res, next) => {
       next();
       return null;
     } catch (err) {
-      res.status(401).send({message: 'user not allowed! you should clear your localstorage and retry!'});
+      res.status(401).send({ message: 'user not allowed! you should clear your localstorage and retry!' });
       return null;
     }
   }
   next();
   return null;
 };
+
